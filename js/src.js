1fb5393b98c53ea8b0c8aceb7246e63321b3f832
Cassidy <icassidyif@gmail.com>
2019-02-08 15:47:38 +0200

    08022019_01

--- js/src.js
+++ js/src.js
@@ -1,1 +1,6 @@
-$('.single-item').slick();
+var mainMenu = document.querySelector('.main-menu');
+var menuShow = document.querySelector('.menu-show');
+// console.log(mainMenu);
+menuShow.addEventListener('click', function(){
+  mainMenu.classList.toggle('expanded');
+})
